47d40ec00bf77af9815b37bbea3c898c278f3a8d
Lizzi <lizaveta9223@gmail.com>
2020-11-08 23:26:33 +0300

    Structute of exception handling created

--- Lab 7/Program.cs
+++ Lab 7/Program.cs
@@ -11,44 +11,100 @@ namespace Lab_7
     {
         static void Main(string[] args)
         {
-            Info ship = new Ship(false, 76);
-            Vehicle boat = new Boat(b: 5);
-            boat.CaptainAge = 55;
-            Vehicle steamer = new Steamer(2001, 156);
-            steamer.CaptainAge = 30;
-            Info corvette = new Corvette("Ususal");
-            VehicleDoes sailboat = new Sailboat("red", "silk", 20);
+            //Info ship = new Ship(false, 76);
+            //Vehicle boat = new Boat(b: 5);
+            //boat.CaptainAge = 55;
+            //Vehicle steamer = new Steamer(2001, 156);
+            //steamer.CaptainAge = 30;
+            //Info corvette = new Corvette("Ususal");
+            //VehicleDoes sailboat = new Sailboat("red", "silk", 20);
 
-            Printer printer = new Printer();
-            ArrayList list = new ArrayList();
-            list.Add(steamer);
-            list.Add(boat);
-            list.Add(ship);
-            list.Add(sailboat);
-            list.Add(corvette);
+            //Printer printer = new Printer();
+            //ArrayList list = new ArrayList();
+            //list.Add(steamer);
+            //list.Add(boat);
+            //list.Add(ship);
+            //list.Add(sailboat);
+            //list.Add(corvette);
 
-            Port port = new Port();
-            port.vehicles = list;
-            port.Show();
-            Info ship2 = new Ship(true, 100);
-            port.AddVehicle(ship2);
-            port.RemoveVehicle(boat);
-            port.Show();
+            //Port port = new Port();
+            //port.vehicles = list;
+            //port.Show();
+            //Info ship2 = new Ship(true, 100);
+            //port.AddVehicle(ship2);
+            //port.RemoveVehicle(boat);
+            //port.Show();
 
-            VehicleDoes sailboat2 = new Sailboat("white", "cotton", 21);
-            port.AddVehicle(sailboat2);
+            //VehicleDoes sailboat2 = new Sailboat("white", "cotton", 21);
+            //port.AddVehicle(sailboat2);
 
-            PortControl control = new PortControl();
-            Console.WriteLine("WATER\t" + control.AverageWater(port));
+            //PortControl control = new PortControl();
+            //Console.WriteLine("WATER\t" + control.AverageWater(port));
 
-            Vehicle steamer2 = new Steamer(1987, 234);
-            steamer2.CaptainAge = 23;
-            Vehicle steamer3 = new Steamer(1930, 91);
-            steamer3.CaptainAge = 78;
-            port.AddVehicle(steamer2);
-            port.AddVehicle(steamer3);
-            Console.WriteLine("Average seats quantity\t" + control.AverageSeatsCount(port));
-            control.CaptainsBelow35(port);
+            //Vehicle steamer2 = new Steamer(1987, 234);
+            //steamer2.CaptainAge = 23;
+            //Vehicle steamer3 = new Steamer(1930, 91);
+            //steamer3.CaptainAge = 78;
+            //port.AddVehicle(steamer2);
+            //port.AddVehicle(steamer3);
+            //Console.WriteLine("Average seats quantity\t" + control.AverageSeatsCount(port));
+            //control.CaptainsBelow35(port);
+
+            try
+            {
+                try
+                {
+                    Vehicle steamer = new Steamer(2567, 156);
+                }
+                catch (YearOfSteamerCreationException ex)
+                {
+                    Console.WriteLine("YearOfSteamerCreationException");
+                }
+                try
+                {
+                    Vehicle boat = new Boat(b : 34);
+                }
+                catch (BoatCapacityException ex)
+                {
+                    Console.WriteLine("BoatCapacityException");
+                }
+                try
+                {
+                    VehicleDoes sailboat2 = new Sailboat("black", "cotton", 21);
+                }
+                catch (SailsColorException ex)
+                {
+                    Console.WriteLine("SailsColorException");
+                }
+                try
+                {                    
+                    ArrayList list = new ArrayList();
+                    Port port = new Port();
+                    port.vehicles = list;
+                    PortControl control = new PortControl();
+                    Console.WriteLine("Average seats quantity\t" + control.AverageSeatsCount(port));
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("DivideByZeroException");
+                }
+                try
+                {
+                    VehicleDoes sailboat = new Sailboat("red", "silk", 100);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("IndexOutOfRangeException");
+                }                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception");
+            }
+            finally
+            {
+                Console.WriteLine("The program is done. It's a finally block.");
+            }
         }
     }
 }
